2ecd3510bd4772accbbc063b41135b6dda709e03
Anastasia <nastia_kim_96@mail.ru>
2021-06-23 22:58:49 +1000

    test1

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,13 @@ const User = require('../db/models/user.model')
 const router = Router()
 
 
-
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if(err) return res.redirect('/')
+    res.clearCookie(req.app.get('cookieName'))
+    return res.redirect('/')
+  })
+})
 
 
 module.exports = router
