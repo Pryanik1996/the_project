c563e8153174f761a9130f6587a7a5b73cc008ac
ruslan <ruslan.rodionow98@gmail.com>
2021-06-23 15:58:40 +0300

    save

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,29 @@
 const { Router } = require('express')
 const User = require('../db/models/user.model')
 const router = Router()
+const SALTROUND = 8;
 
 
-
-
+router
+  .route('/registration')
+  .get((req, res) => {
+    res.render('registration');
+  })
+  .post(async (req, res) => {
+    const { name, email, password } = req.body;
+    const hash = await bcrypt.hash(password, SALTROUND);
+    try {
+      const newUser = await User.create({ name, email, password: hash });
+      console.log('newUser =>', newUser);
+      if (newUser) {
+        req.session.username = newUser.name;
+        req.session.userId = newUser._id;
+      }
+      res.redirect('/');
+    } catch (error) {
+      console.log(error);
+      res.redirect('/users/registration');
+    }
+  });
 
 module.exports = router
