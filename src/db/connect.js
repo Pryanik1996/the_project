164c65b8c1566178dcf434df898d3d93b38b8636
ruslan <ruslan.rodionow98@gmail.com>
2021-06-23 15:40:30 +0300

    save

--- src/db/connect.js
+++ src/db/connect.js
@@ -12,12 +12,4 @@ function disconnect() {
 
 module.exports = { connect, disconnect };
 
-const { dbUrl, options } = require("./config");
-const mongoose = require("mongoose");
 
-const connect = () =>
-	mongoose.connect(dbUrl, options).then(() => console.log("Connect to DB"));
-
-const disconnect = () => mongoose.disconnect();
-
-module.exports = { connectD, disconnect };
