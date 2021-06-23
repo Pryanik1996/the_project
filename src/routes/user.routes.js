d12b56835e8c2cd9baa5c0bd39c0bb8b91b14198
Kirill <kirillnemkov98@gmail.com>
2021-06-23 16:20:50 +0300

    nothing new

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
-const { Router } = require('express')
-const User = require('../db/models/user.model')
-const bcrypt = require('bcrypt');
-const router = Router()
+const { Router } = require("express");
+const User = require("../db/models/user.model");
+const bcrypt = require("bcrypt");
+const router = Router();
 const SALTROUND = 8;
 
 router
@@ -9,12 +9,12 @@ router
   .get((req, res) => {
     res.render("login");
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     const { email, password } = req.body;
     try {
       const findUser = await User.findOne({ email });
       if (findUser && (await bcrypt.compare(password, findUser.password))) {
-        req.session.username = findUser.name;
+        req.session.userName = findUser.name;
         req.session.userId = findUser._id;
         res.redirect("/");
       }
@@ -25,34 +25,33 @@ router
   });
 
 router
-  .route('/registration')
+  .route("/registration")
   .get((req, res) => {
-    res.render('registration');
+    res.render("registration");
   })
   .post(async (req, res) => {
     const { name, email, password } = req.body;
     const hash = await bcrypt.hash(password, SALTROUND);
     try {
       const newUser = await User.create({ name, email, password: hash });
-      console.log('newUser =>', newUser);
+      console.log("newUser =>", newUser);
       if (newUser) {
         req.session.userName = newUser.name;
         req.session.userId = newUser._id;
       }
-      res.redirect('/');
+      res.redirect("/");
     } catch (error) {
       console.log(error);
-      res.redirect('/users/registration');
+      res.redirect("/users/registration");
     }
   });
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.session.destroy((err) => {
-    if(err) return res.redirect('/')
-    res.clearCookie(req.app.get('cookieName'))
-    return res.redirect('/')
-  })
-})
+    if (err) return res.redirect("/");
+    res.clearCookie(req.app.get("cookieName"));
+    return res.redirect("/");
+  });
+});
 
-
-module.exports = router
+module.exports = router;
