dbc07ab56ee5c9ff5030c236a29c284ceaa126fa
ruslan <ruslan.rodionow98@gmail.com>
2021-06-25 17:50:32 +0300

    f

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const hbs = require("hbs");
 const path = require("path");
 const { mongoUrl } = require("./src/db/config");
 const { connect } = require("./src/db/connect");
-const sessions = require('express-session')
-
+const sessions = require("express-session");
 
 const indexRoute = require("./src/routes/index.routes");
 const userRoute = require("./src/routes/user.routes");
@@ -20,15 +19,14 @@ const app = express();
 const PORT = 3000;
 
 hbs.registerPartials(path.join(process.env.PWD, "src", "views", "partials"));
-hbs.registerHelper('addEditDelete', (owner, idUser) => {
-  if(String(owner) === userId) {
-    return new hbs.SafeString(`
+hbs.registerHelper("addEditDelete", (owner, idUser) => {
+	if (String(owner) === userId) {
+		return new hbs.SafeString(`
     <a href="/posts/edit/{{this._id}}" class="btn btn-danger edit">Edit</a>
     <button class="btn btn-danger delete">Delete</button>
-    `)
-  }
-}) 
-
+    `);
+	}
+});
 
 app.set("views", path.join(__dirname, "src", "views"));
 app.set("view engine", "hbs");
@@ -37,36 +35,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const sessionParser = sessions({
-  name: app.get('cookieName'),
-  secret: 'e1d322030d13b4d27cac8244491c65f4e86aab35819c8b922618b9ad0f6e846a49ee9199590abc1562b41f992543df897ea386b86666458e4ca6d9f73bdd5f9c',
-  resave: false, // Не сохранять сессию, если мы ее не изменим
-  saveUninitialized: false, // не сохранять пустую сессию
-  // store: new FileStore({ // выбираем в качестве хранилища файловую систему
-  //   secret: secretKey,
-  // }),
-  store: MongoStore.create({ // выбираем в качестве хранилища mongoDB
-    mongoUrl: 'mongodb://localhost:27017/AVITO',
-  }),
-  cookie: { // настройки, необходимые для корректного работы cookie
-    httpOnly: true, // не разрещаем модифицировать данную cookie через javascript
-    // maxAge: 100000000, // устанавливаем время жизни cookie
-
-  },
+	name: app.get("cookieName"),
+	secret:
+		"e1d322030d13b4d27cac8244491c65f4e86aab35819c8b922618b9ad0f6e846a49ee9199590abc1562b41f992543df897ea386b86666458e4ca6d9f73bdd5f9c",
+	resave: false, // Не сохранять сессию, если мы ее не изменим
+	saveUninitialized: false, // не сохранять пустую сессию
+	// store: new FileStore({ // выбираем в качестве хранилища файловую систему
+	//   secret: secretKey,
+	// }),
+	store: MongoStore.create({
+		// выбираем в качестве хранилища mongoDB
+		mongoUrl,
+	}),
+	cookie: {
+		// настройки, необходимые для корректного работы cookie
+		httpOnly: true, // не разрещаем модифицировать данную cookie через javascript
+		// maxAge: 100000000, // устанавливаем время жизни cookie
+	},
 });
 app.use(sessionParser);
 
-
-app.set('cookieName', 'userCookie') // Устанавливаем в настройках сервера специальную переменную
-
-
-
+app.set("cookieName", "userCookie"); // Устанавливаем в настройках сервера специальную переменную
 
 app.use((req, res, next) => {
 	res.locals.userName = req.session.userName;
 	res.locals.userId = req.session.userId;
 	res.locals.userAdmin = req.session.userAdmin;
-  console.log(res.locals.userId )
-  console.log(	res.locals.userAdmin)
+	console.log(res.locals.userId);
+	console.log(res.locals.userAdmin);
 	next();
 });
 
